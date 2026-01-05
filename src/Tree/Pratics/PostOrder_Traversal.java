1c18fdb36e6006e290c6defe5e5499fe72f33396
Aagam33gitcode <aagamjain192@gmail.com>
2026-01-05 21:50:14 +0530

    Tree 4

--- src/Tree/Pratics/PostOrder_Traversal.java
+++ src/Tree/Pratics/PostOrder_Traversal.java
@@ -1,9 +1,11 @@
 package Tree.Pratics;
 
 import com.sun.source.tree.Tree;
+import com.sun.source.tree.WhileLoopTree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class PostOrder_Traversal {
     public class TreeNode {
@@ -35,6 +37,27 @@ public class PostOrder_Traversal {
         postOrder(root.left,ans);
         postOrder(root.right,ans);
         ans.add(root.val);
+        }
+        public List<Integer> postOrderItreative(TreeNode root){
+        List<Integer> ans=new ArrayList<>();
+            Stack<TreeNode> st=new Stack<>();
+            TreeNode node=root;
+            while(node!=null||!st.isEmpty()){
+                while (node!=null){
+                    st.push(node);
+                    node=node.left;
+                }
+                if (st.peek().right!=null){
+                    node=st.peek().right;
+                }else{
+                    TreeNode temp=st.pop();
+                    ans.add(temp.val);
+                    while (!st.isEmpty() && temp==st.peek().right){
+                    temp=st.pop();
+                    ans.add(temp.val);
+                    }
+                }
+            }return ans;
         }
     }
 
