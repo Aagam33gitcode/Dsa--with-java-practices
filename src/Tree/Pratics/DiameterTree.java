d37ed650f49f5be4ca4e82cc146de9c1086f34a6
Aagam33gitcode <aagamjain192@gmail.com>
2026-01-08 23:28:57 +0530

    Tree 6

---
+++ src/Tree/Pratics/DiameterTree.java
@@ -0,0 +1,53 @@
+package Tree.Pratics;
+
+public class DiameterTree {
+//      Definition for a binary tree node.
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+      public int diameterTree(TreeNode root){
+          if (root==null){
+              return 0;
+          }
+          int lheigth=diameterTree(root.left);
+          int rheigth=diameterTree(root.right);
+          int selfheigth=ht(root.left)+ ht(root.right)+2;
+          return Math.max(lheigth,Math.max(rheigth,selfheigth));
+      }
+
+    private int ht(TreeNode node) {
+          if (node==null){
+              return -1;
+          }
+          int left=ht(node.left);
+          int right=ht(node.right);
+          return Math.max(left,right)+1;
+    }
+
+class DiaPair{
+          int dt=0;
+          int ht=-1;
+
+}
+public DiaPair diameter1(TreeNode root){
+          if(root==null){
+              return new DiaPair();
+          }
+          DiaPair ldp=diameter1(root.left);
+          DiaPair rdp=diameter1(root.right);
+          DiaPair sdp=new DiaPair();
+          int sd=ldp.ht+rdp.ht+2;
+          sdp.dt =Math.max(sd,Math.max(ldp.dt,rdp.dt));
+          sdp.ht=Math.max(ldp.ht,rdp.ht)+1;
+          return sdp;
+}
+}
